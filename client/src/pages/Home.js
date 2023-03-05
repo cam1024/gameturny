73f3e5bd97aa0ebc103b97fa9f821d70bef9b27f
saitek <saitek300@gmail.com>
2023-03-04 21:36:44 -0800

    building Modal and searchbar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Button, Form, Col, Row, Container } from 'react-bootstrap';
+
 
 const Home = () => {
   
   return (
-    <div className="card bg-white card-rounded w-50">
-      HOME
-    </div>
+    <Container fluid>
+      <Form>
+        <Row>
+          <Col  xs={12} md={10}>
+            <Form.Control type='search' placeholder='Search Games here' />
+          </Col>
+          <Col xs={12} md={2}>
+            <Button type='submit' variant="info" size='md'>Search</Button>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 };
 
