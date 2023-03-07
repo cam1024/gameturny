c3ecb06296a88f3860ae8d8ef60f7767d7280783
saitek <saitek300@gmail.com>
2023-03-06 21:21:13 -0800

    add ADD/DELETE GAME mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,37 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const ADD_GAME = gql`
+mutation addGame($Game:GameSearch!) {
+    addGame(Game: $Game){
+        _id
+        username
+        email
+        myGames {
+            game_id
+            name
+            background_image
+            description
+            genre
+        }
+    }
+}
+`;
+
+export const DELETE_GAME =gql`
+mutation deleteGame($game_id: ID!) {
+    deleteGame(game_id: $game_id) {
+        _id
+        username
+        email
+        myGames {
+            game_id
+            name
+            background_image
+            description
+            genre
+        }
+    }
+}
+`
