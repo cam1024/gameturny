b041c736b1e7226b0b09663289f155648ee9bb4e
Roger Kinney <rogerkinney@Rogers-MacBook-Air.local>
2023-03-06 21:48:05 -0800

    API Catch & If/Then

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,13 @@
 export const searchGame = (query) => {
-    return fetch(`https://api.rawg.io/api/games?search=${encodeURIComponent(query)}`);
-  };
+    return fetch(`https://api.rawg.io/api/games?search=${encodeURIComponent(query)}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Errror('Network response failed');
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error('There was an issue with the fetch')
+        throw error;
+      })
+    };
