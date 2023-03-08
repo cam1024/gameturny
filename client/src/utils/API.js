e7d9663b8e813728eeb1d320926837e2b19ad9fe
saitek <saitek300@gmail.com>
2023-03-07 23:40:41 -0800

    bug fix API fetching issue

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 export const searchGame = (query) => {
-    return fetch(`https://api.rawg.io/api/games?search=${encodeURIComponent(query)}`);
-  };
+  return fetch(`https://api.rawg.io/api/games?key=3cef6cdbeed5482aaa8ce3f7cdd1f83b&search=${query}`);
+};
