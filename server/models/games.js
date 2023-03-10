ac55292268170d851998003a3c05c313c043298a
saitek <saitek300@gmail.com>
2023-03-09 17:22:56 -0800

    game model change

--- server/models/games.js
+++ server/models/games.js
@@ -2,11 +2,15 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const gameSchema = new Schema({
-    id: {
+    game_id: {
     type: String,
     required: true,
   },
-     name: {
+     title: {
+    type: String,
+    required: true,
+  },
+  description: {
     type: String,
     required: true,
   },
